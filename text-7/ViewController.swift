7b89612f3be2518a5a95fe68b76e2ea469b25c00
Cynthia19990222 <zxcccc987@qq.com>
2018-10-13 08:49:39 +0800

    Initial Commit

---
+++ text-7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  text-7
+//
+//  Created by s20171103197 on 2018/10/13.
+//  Copyright © 2018年 s20171103197. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
